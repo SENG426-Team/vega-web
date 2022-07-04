9f4ac2dbf9dddf72a496e261cd00e242f7c90cda
Chloe Blankers <60280561+chloe-blankers@users.noreply.github.com>
2022-07-04 11:12:33 -0700

    Update AdminPanel.js

--- src/components/pages/AdminPanel.js
+++ src/components/pages/AdminPanel.js
@@ -28,7 +28,7 @@ const AdminPanel = (props) => {
 
 	const changeRole = (evt, username) => {
 		if(username === "admin@venus.com"){
-			alert("Admin user is not permitted to change roles.")
+			alert("Admin user is not permitted to change it's own role.")
 			evt.target.value = "Default"
 		} else {
 			console.log(evt.target.value, username)
