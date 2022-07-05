74ced715f7957ad1819901e1cacdaca29a1cb9ee
TyRicard <tricard.summer@gmail.com>
2022-07-04 18:54:10 -0700

    Adding error messages

--- src/components/pages/RegisterAccount.js
+++ src/components/pages/RegisterAccount.js
@@ -8,25 +8,38 @@ import {createAccount} from '../../service/AccountHandler/AccountHandler.js';
 const RegisterAccount = (props) => {
 
 	const { context } = props;
-	const {user, setUserInfo,logout} = useContext(UserContext);
-	const [username, setUsername]  = useState('');
-	const [password, setPassword] = useState('');
-	const [firstname, setFirstname]  = useState('');
-	const [lastname, setLastname] = useState('');
-	const [auth, setAuth] = useState(false);
+	const [username, setUsername]  = useState("");
+	const [password, setPassword] = useState("");
+	const [firstname, setFirstname]  = useState("");
+	const [lastname, setLastname] = useState("");
+	const [errorMSG, setErrorMSG] = useState("");
+	const [successMSG, setSuccessMSG] = useState("");
 	
 	const handleSubmission = () => {
-		const registration_data = {
-							"username": username,
-							"firstname": firstname,
-							"lastname": lastname,
-							"password": password
-							};
+		if (username == "" || firstname == "" || lastname == "" || password == "") {
+			console.log("Input Error");
+			setErrorMSG("Registration Error: An Input Field Was Left Empty");
+		} else {
+			const registration_data = {
+								"username": username,
+								"firstname": firstname,
+								"lastname": lastname,
+								"password": password
+								};
 
-		createAccount(registration_data)
-		.then(resp => {
-			console.log("Response", resp);
-		})
+			createAccount(registration_data)
+			.then(resp => {
+				if(resp.code == 401){
+					console.log("Registration Error");
+					setErrorMSG("Registration Error: Username is not available.");
+					setSuccessMSG("")
+				} else {
+					console.log("Response", resp);
+					setErrorMSG("");
+					setSuccessMSG("Account Registered. Note, Admin Must Enable The Account.")
+				}
+			})
+		}
 	}
 
 			return (
@@ -53,6 +66,9 @@ const RegisterAccount = (props) => {
 						<Button variant="primary" type="submit" onClick={handleSubmission}>
 							Submit
 						</Button>
+						<br/>
+						<p class="text-danger">{errorMSG}</p>
+						<p class="text-success">{successMSG}</p>
 					</Col>
 				</Row>
 				</SimplePageLayout>
