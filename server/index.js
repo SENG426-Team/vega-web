b863d0933db2290a9f015473c0e7cd2fdde6ce45
Isabella Rojas <68630621+isabellarojasg@users.noreply.github.com>
2022-08-03 12:14:04 -0700

    adding helmet package

--- server/index.js
+++ server/index.js
@@ -7,6 +7,9 @@ import express from 'express';
 import { config } from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import helmet from "helmet";
+
+
 
 const app = express();
 const port = 8000;
@@ -15,6 +18,7 @@ const env = config();
 //app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({limit: '50mb'}));
+app.use(helmet());
 app.disable('x-powered-by');
 
 
